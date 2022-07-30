5bcca4570cb36348de511fd966aaebcd90f7f777
AyushiPohekar <ayushi2021.29@gmail.com>
2022-07-31 02:26:59 +0530

    api of heroku added

--- src/global.js
+++ src/global.js
@@ -1,4 +1,3 @@
 // export const API="https://62a970daec36bf40bdb78cff.mockapi.io"
 
-// export const API="https://crudayushiapp.herokuapp.com"
-export const API="http://localhost:4000"
+export const API="https://crudayushiapp.herokuapp.com"
